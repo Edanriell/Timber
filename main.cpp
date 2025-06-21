4744d0443b848a0f59722bea5a8f410c6ed08708
Edanriell <Edanriell@gmail.com>
2025-06-21 16:06:02 +0300

    feature(initial-config): minor fixes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+// Include important libraries here
+#include <SFML/Graphics.hpp>
+
+// Make code easier to type with "using namespace"
+using namespace sf;
 
 // This is where our game starts from
 int main() {
+    // Create a video mode object
+    VideoMode vm(Vector2u(1920, 1080));
+
+    // Create and open a window for the game
+    RenderWindow window(vm, "Timber!!!", State::Windowed);
+
     return 0;
 }
